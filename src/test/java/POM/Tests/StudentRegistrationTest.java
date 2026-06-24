fb884c027ce7d4706eb431b0bf02392e286d1a70
amartejac <Amarteja06@gmail.com>
2026-06-24 20:42:37 +0530

    testing purpose reversed

--- src/test/java/POM/Tests/StudentRegistrationTest.java
+++ src/test/java/POM/Tests/StudentRegistrationTest.java
@@ -36,7 +36,7 @@ public class StudentRegistrationTest extends BaseTest {
         try {
 
             signup.clickSignup();
-            signup.enterProgramCode("");
+            signup.enterProgramCode("228614");
             signup.continueRegistration();
 
             traveller.selectStudent();
